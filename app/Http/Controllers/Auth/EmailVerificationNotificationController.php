503df7e6c59e1f073e43d7959a09aed0739857ed
adityaaditiya <48347372+adityaaditiya@users.noreply.github.com>
2026-03-07 22:22:57 +0700

    Add user my schedule page and link from landing navbar

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -14,7 +14,9 @@ class EmailVerificationNotificationController extends Controller
     public function store(Request $request): RedirectResponse
     {
         if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(route('dashboard', absolute: false));
+            $redirectRoute = $request->user()->hasRole('customer') ? 'welcome' : 'dashboard';
+
+            return redirect()->intended(route($redirectRoute, absolute: false));
         }
 
         $request->user()->sendEmailVerificationNotification();
--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -14,14 +14,16 @@ class VerifyEmailController extends Controller
      */
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
+        $redirectRoute = $request->user()->hasRole('customer') ? 'welcome' : 'dashboard';
+
         if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
+            return redirect()->intended(route($redirectRoute, absolute: false).'?verified=1');
         }
 
         if ($request->user()->markEmailAsVerified()) {
             event(new Verified($request->user()));
         }
 
-        return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
+        return redirect()->intended(route($redirectRoute, absolute: false).'?verified=1');
     }
 }
