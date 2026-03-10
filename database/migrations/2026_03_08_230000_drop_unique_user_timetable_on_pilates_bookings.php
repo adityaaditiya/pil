faf1ef53a671599df7040fa7fadcd48373bee619
adityaaditiya <adityamuhammad9@gmail.com>
2026-03-11 03:45:27 +0700

    drop unique user timetable

--- database/migrations/2026_03_08_230000_drop_unique_user_timetable_on_pilates_bookings.php
+++ database/migrations/2026_03_08_230000_drop_unique_user_timetable_on_pilates_bookings.php
@@ -1,26 +1,3 @@
-<!-- < ?php -->
-
-<!-- use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
-return new class extends Migration
-{
-    public function up(): void
-    {
-        Schema::table('pilates_bookings', function (Blueprint $table) {
-            $table->dropUnique('pilates_bookings_user_id_timetable_id_unique');
-        });
-    }
-
-    public function down(): void
-    {
-        Schema::table('pilates_bookings', function (Blueprint $table) {
-            $table->unique(['user_id', 'timetable_id']);
-        });
-    }
-// }; -->
-
 <?php
 
 use Illuminate\Database\Migrations\Migration;
